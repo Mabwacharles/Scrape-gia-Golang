738943ee5feef22a767d34eb0b00bedce5edc434
Charles Mabwa <mabwacharles@gmail.com>
2022-03-16 17:41:54 +0300

    Add dependencies

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
@@ -10,6 +12,19 @@ func main() {
 
 	// Create a new request using http
 	response, error := http.Get(url)
+	defer response.Body.Close()
+
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	if response.StatusCode == 200 {
+		fmt.Println("Successfully retrieved", url)
+	} else {
+		fmt.Println("Couldn't retrieve", url)
+	}
+
+	doc, error := goquery.NewDocumentFromReader(response.Body)
 	if error != nil {
 		fmt.Println(error)
 	}
